9bc1dcb08268be31d267f68833f292847cf67c14
wuqinghua <1632726788@qq.com>
2021-09-26 18:53:35 +0800

    a

---
+++ src/components/home/carousel/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="carousel">
+        <NCarousel autoplay>
+            <img class="car-img" src="@/assets/images/one.jpg">
+            <img class="car-img" src="@/assets/images/two.png">
+            <img class="car-img" src="@/assets/images/three.png">
+            <img class="car-img" src="@/assets/images/four.png">
+        </NCarousel>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.carousel {
+    width: 100%;
+    height: 200px;
+    background-color: green;
+
+    .car-img {
+        width: 100%;
+    }
+}
+</style>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import { NCarousel } from "naive-ui"
+
+export default defineComponent({
+    components: {
+        NCarousel
+    },
+    setup() {
+        return {}
+    },
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,33 @@
   <div class="home">
     <Head />
     <Nav />
+    <div class="main">
+      <Carousel />
+    </div>
   </div>
   
 </template>
+
+<style lang="less" scoped>
+.main {
+  box-sizing: border-box;
+  width: 95%;
+  margin: 5px 2.5%;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Head from "@/components/home/head/Head.vue"
 import Nav from "@/components/home/head/Nav.vue"
+import Carousel from "@/components/home/carousel/Carousel.vue"
 
 export default defineComponent({
   name: 'Home',
   components: {
     Head,
-    Nav
+    Nav,
+    Carousel
   },
   setup() {
     return {}
